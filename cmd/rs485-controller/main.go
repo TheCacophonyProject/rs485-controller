fc0988ccb587eef1662fb2c95c311d473472d192
Cameron Ryan-Pears <cameron.ryan.pears@gmail.com>
2019-04-10 13:59:52 +1200

    Power on trap through RPi HAT now.

--- cmd/rs485-controller/main.go
+++ cmd/rs485-controller/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 
 	trapController "github.com/TheCacophonyProject/rs485-controller/trapController"
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/host"
 )
 
 var version = "<not set>"
+
+const powerPin = "GPIO27"
 
 func main() {
 
 	log.SetFlags(0) // Removes timestamp output
 	log.Printf("running version: %s", version)
+
+	// Turn power on to traps
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+	restartTrapPower(powerPin)
 
 	//TODO make this an array of devices
 	device, err := trapController.NewTrap("./trap-config.yaml", "/dev/ttyAMA0", 9600, 3000)
@@ -43,4 +55,17 @@ func main() {
 		log.Println(device.Actuators[0].Retracted)
 		time.Sleep(60 * time.Second)
 	}
+}
+
+func restartTrapPower(powerPin string) error {
+	log.Println("restarting trap power")
+	pin := gpioreg.ByName(powerPin)
+	if pin == nil {
+		fmt.Errorf("no '%s' pin found", powerPin)
+	}
+	pin.Out(gpio.Low)
+	time.Sleep(time.Second * 4)
+	pin.Out(gpio.High)
+	time.Sleep(time.Second * 2)
+	return nil
 }
